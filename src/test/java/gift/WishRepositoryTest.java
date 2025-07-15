fe4a6acfd88fa5eee56a8dd30d0384136396710e
kimpro4214 <qnfghk555@naver.com>
2025-07-15 14:41:33 +0900

    feat: WishRepositoryTest 추가

---
+++ src/test/java/gift/WishRepositoryTest.java
@@ -0,0 +1,40 @@
+package gift;
+
+import gift.entity.Member;
+import gift.entity.Product;
+import gift.entity.Wish;
+import gift.repository.MemberRepository;
+import gift.repository.ProductRepository;
+import gift.repository.WishRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class WishRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private WishRepository wishRepository;
+
+    @Test
+    void save() {
+        Member member = new Member("wish@example.com", "pw123");
+        memberRepository.save(member);
+
+        Product product = new Product("mouse", "http://image.com/mouse", 20000);
+        productRepository.save(product);
+
+        Wish wish = new Wish(member, product);
+        Wish saved = wishRepository.save(wish);
+
+        assertThat(saved.getId()).isNotNull();
+    }
+}
