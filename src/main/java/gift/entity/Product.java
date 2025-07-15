7f944a9c0a9fd8ade1fb106394c2c663608fdeab
kimpro4214 <qnfghk555@naver.com>
2025-07-15 14:31:56 +0900

    refactor: Product 엔티티 수정

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -1,61 +1,39 @@
 package gift.entity;
 
-import gift.dto.ProductRequestDto;
-import gift.exception.InvalidProductNameException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "product")
 public class Product {
 
-    private static final String BLOCKED_PRODUCT_NAME_KEYWORD = "카카오";
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 15)
     private String name;
-    private int price;
+
+    @Column(nullable = false)
     private String imageUrl;
 
-    public Product(Long id, String name, int price, String imageUrl) {
-        validateName(name);
-        this.id = id;
+    @Column(nullable = false)
+    private Integer price;
+
+    protected Product() {
+    }
+
+    public Product(String name, String imageUrl, Integer price) {
         this.name = name;
-        this.price = price;
         this.imageUrl = imageUrl;
-    }
-
-    public Product() {
-    }
-
-
-    public Product(String name, int price, String imageUrl) {
-        this(null, name, price, imageUrl);
-    }
-
-    public static Product from(ProductRequestDto dto) {
-        return new Product(dto.name(), dto.price(), dto.imageUrl());
-    }
-
-    private void validateName(String name) {
-        if (name.contains(BLOCKED_PRODUCT_NAME_KEYWORD)) {
-            throw new InvalidProductNameException("상품 이름에 '" + BLOCKED_PRODUCT_NAME_KEYWORD + "'를 포함할 수 없습니다. 담당 MD와 협의 필요");
-        }
+        this.price = price;
     }
 
     public Long getId() {
         return id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
 }
